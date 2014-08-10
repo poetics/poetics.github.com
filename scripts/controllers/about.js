7dd3598cbcfe82d7fc128c31d0d2bd72061a4230
Gerald Lee Jordan <geraldleejordan@gmail.com>
2014-08-10 17:32:24 +1200

    First new site files from Yeoman

---
+++ scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name poeticsgithubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the poeticsgithubioApp
+ */
+angular.module('poeticsgithubioApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
